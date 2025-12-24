76789822c939be93ba12dec7e38f2e35c6fe1125
Sam-TN <meysamtaherinia76@gmail.com>
2025-12-24 18:34:50 +0330

    first commit

---
+++ src/main/java/com/tromaya/simple_spring_boot_template/SimpleSpringBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.tromaya.simple_spring_boot_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tromaya/simple_spring_boot_template/SimpleSpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tromaya.simple_spring_boot_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
